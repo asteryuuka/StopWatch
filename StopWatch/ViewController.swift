276a1811af4f8bca4dfdd9161f74d43c64336850
Yuuka Kaneko <0416yuuka@gmail.com>
2019-10-19 17:56:20 +0900

    Fixed Bug

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count: Double = 0.00
+    var timer: Timer!
 
+    @IBOutlet var timerLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func start() {
+        
+        //0.01秒ごとにこのファイル内のupdate関数をよび、それを毎0.01秒ずつ繰り返す
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(UIMenuController.update), userInfo: nil, repeats: true)
+        
+    }
 
+    
+    @IBAction func stop() {
+        timer.invalidate()
+    }
+    
+    func update () {
+        count = count + 0.01
+        //フォーマット指定子
+        timerLabel.text = String(format: "%.2f", count)
+    }
 
 }
 
